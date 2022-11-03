42f78762a6786dd3fe7fea9dc2d9f278cef41017
SammyWebdev <sammy.webdeveloper@gmail.com>
2022-11-03 22:18:01 +0100

    practicality stripdown (only what's needed stays)

--- main.py
+++ main.py
@@ -7,12 +7,6 @@
 #   Der Methodenaufruf <print_per_uid_ptty(name=True)> ist nicht notwendig und nur zur Veranschaulichung der erhaltenen Daten.
 # ----------------------------------------------------------------------------------
 
-
-# TODO fixes:
-# - somethings I've missed :)
-
-print("\n\n" + "<" + "=" * 150 + ">")
-
 import requests
 import json
 
@@ -21,19 +15,9 @@ overpass_url = "http://overpass-api.de/api/interpreter"
 search_radius = 500
 max_search_radius = 2000
 
-# user id's (or search id's, probably a better name :) TODO rename
-uids = {
-    36862660,
-    86953877,
-    678537601,
-    678537289,
-    678537291,
-    3870914569,
-}  # obtained from Fabi, this is just for sample testing
+# user id's (or search id's)
+uids = {}
 
-# uids = {678537289, 3870914569}  # for request easy testing TODO remove
-
-include_searchplace = False  # should stay that way
 results = {}  # dict<uid: query_result["elements"]>
 
 print("Fetching Data...", end="\n" * 2)
@@ -42,17 +26,11 @@ for uid in uids:
     while search_radius < max_search_radius:
         overpass_query = """
         [out:json][timeout:500];
-        nwr(id:{0}){2}
+        nwr(id:{0})->.res;
         nwr[amenity=bar](around.res:{1});
-        nwr[amenity=pub](around.res:{1});
-        nwr[amenity=biergarten](around.res:{1});
-        nwr[shop=alcohol](around.res:{1});
-        nwr[shop=beverages](around.res:{1});
         out center;
         """.format(
-            uid,
-            search_radius,
-            ";nwr._->.res;out center;" if include_searchplace else "->.res;",
+            uid, search_radius
         )
         try:
             response = requests.get(overpass_url, params={"data": overpass_query})
@@ -69,14 +47,10 @@ for uid in uids:
                 )
             )  # 400 is bad request, 429 is too many requests, 200 is ok
 
-        # debugging status codes
-        # print(f"status: {response.status_code}")
-
         # dealing with empty result
         if result["elements"] == []:
             search_radius += 500  # expanding search radius
         else:
-            # print("debug", result)
             break  # stops further querying
 
     results[uid] = (
@@ -84,70 +58,3 @@ for uid in uids:
         if result["elements"] == []
         else result["elements"]
     )
-
-
-def print_per_uid():
-    for uid in uids:
-        print(
-            f"(uid={uid}):",
-            str(results[uid])[:1000],
-            "\b..." if len(str(str(results[uid]))) > 999 else "\b",
-            end="\n" * 2,
-        )
-
-
-def print_per_uid_ptty(name=False):  # pretty prints the results
-    for uid in uids:
-        print(f"(uid={uid}):")
-        try:
-            for element in results[uid]:
-                if name:
-                    print(
-                        "\t{} (eid={})".format(element["tags"]["name"], element["id"]),
-                        end="",
-                    )
-                    try:
-                        found_addr = False
-                        addr_string = ', Address: "'
-                        print(addr_string, end="")
-                        addr = {  # possible address components
-                            "city": "_",
-                            "country": "_",
-                            "housenumber": "_",
-                            "postcode": "_",
-                            "street": "_",
-                        }
-                        tags = element["tags"]
-                        for tag_key, _ in tags.items():
-                            if tag_key[:4] == "addr":  # finds address components
-                                found_addr = True
-                                addr[tag_key[5:]] = tags[tag_key]
-                                # print("debug:", addr, tag_key)
-                            else:
-                                pass  # key isn't part of an address
-                        if found_addr:
-                            print(
-                                addr["street"],
-                                addr["housenumber"],
-                                addr["postcode"],
-                                addr["city"],
-                                end='"\n',
-                            )
-                        else:
-                            print('No Address given."')
-                    except:
-                        # print("Element doesn't have tags")
-                        pass
-                else:
-                    pass
-                    # print("\t(eid={}): {}".format(element.get("id"), element))
-        except:
-            print(f"\t{results[uid]}")  # when there are no results
-
-        print("-" * 50)
-
-
-print_per_uid_ptty(name=True)
-
-# debugging
-# print(results)

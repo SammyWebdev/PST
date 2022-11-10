5022757a5d43cb0e1fc73295e4b379c20fe2111e
SammyWebdev <sammy.webdeveloper@gmail.com>
2022-11-10 18:32:49 +0100

    fixed osm.py

--- main.py
+++ main.py
@@ -1,147 +1,166 @@
-#import
+# import
 import csv
 import sys
 import os
-#import pandas
+
+# import pandas
 import quopri
 import pandas as pd
 import geo_coding
 import osm
+
 # variablen daklaration
 
 Kontakt_new = []
-Kontakt=[]
-csv_file = os.path.abspath(".")+'/Kontakte.csv'
-vcf_file= os.path.abspath(".")+'/2022-10-06_151740.vcf'
-#df_Kontakte_old = pd.read_csv(csv_file)
+Kontakt = []
+csv_file = os.path.abspath(".") + "/Kontakte.csv"
+vcf_file = os.path.abspath(".") + "/2022-10-06_151740.vcf"
+# df_Kontakte_old = pd.read_csv(csv_file)
 
 print("Programm start")
-#_______________decodiert die Verschlüsstelten Adress Daten aus dem vcf File
-def decode_adr(vcf_data_list,i):
-    #print('dekodiere adresse')
-    if vcf_data_list[i + 1][0] == '=': #prüft ob die Codierte message über mehrere Zeilen geht
-        #geht die Codierte Adresse über mehrere Zeilen
-        #werden diese in eine Variablen zusammengefasst um diese dann zu Decodieren
-        print('mehrere zeilen')
-        adress_data = (vcf_data_list[i].split(':')[1] + vcf_data_list[i + 1][1:])
+# _______________decodiert die Verschlüsstelten Adress Daten aus dem vcf File
+def decode_adr(vcf_data_list, i):
+    # print('dekodiere adresse')
+    if (
+        vcf_data_list[i + 1][0] == "="
+    ):  # prüft ob die Codierte message über mehrere Zeilen geht
+        # geht die Codierte Adresse über mehrere Zeilen
+        # werden diese in eine Variablen zusammengefasst um diese dann zu Decodieren
+        print("mehrere zeilen")
+        adress_data = vcf_data_list[i].split(":")[1] + vcf_data_list[i + 1][1:]
         adress = decode_QP(adress_data)
     else:
-        adress = decode_QP(vcf_data_list[i].split(':')[1])
-       # print(adress)
+        adress = decode_QP(vcf_data_list[i].split(":")[1])
+    # print(adress)
     return adress
-#__________________________________________decodiert den QP code und gibt diesen als String zurück____________________
+
+
+# __________________________________________decodiert den QP code und gibt diesen als String zurück____________________
 def decode_QP(str_verschlüsselt):
     temp3 = quopri.decodestring(str_verschlüsselt)
-    temp3 = temp3.decode('utf-8')
-    temp3 = temp3.split(';')[2]
+    temp3 = temp3.decode("utf-8")
+    temp3 = temp3.split(";")[2]
     return temp3
-#________________________________________liest die Kontaktdatei vom Handy aus______________________
+
+
+# ________________________________________liest die Kontaktdatei vom Handy aus______________________
 def vcf_read():
     data = {
-        'Name': [None],
-        'Tel': [None],
-        'Street': [None],
-        'Hausnummer': [None],
-        'Ort': [None],
-        'Plz': [None]
+        "Name": [None],
+        "Tel": [None],
+        "Street": [None],
+        "Hausnummer": [None],
+        "Ort": [None],
+        "Plz": [None],
     }
     dfKontakte = pd.read_csv(csv_file)
-    #print(dfKontakte)
-    with open(vcf_file, mode='r') as vcf: #öffnet die vcf datei mit den Kontakte
-        vcf_data = vcf.read()   #liest vcf file aus
-        vcf_data_list = vcf_data.splitlines()#trennt ausgelesenden file nach zeilen
+    # print(dfKontakte)
+    with open(vcf_file, mode="r") as vcf:  # öffnet die vcf datei mit den Kontakte
+        vcf_data = vcf.read()  # liest vcf file aus
+        vcf_data_list = vcf_data.splitlines()  # trennt ausgelesenden file nach zeilen
         i = 0
         adresse = []
-        while i+1 < len(vcf_data_list) :#durchläuft einmal ganze liste
-            name = 's'
-            #tel='s'
+        while i + 1 < len(vcf_data_list):  # durchläuft einmal ganze liste
+            name = "s"
+            # tel='s'
             try:
 
-                if(vcf_data_list[i][0]+(vcf_data_list[i][1])+(vcf_data_list[i][2]))== 'ADR': #sucht Positionen die mit ADR beginnen in Liste (Zeichen für Adresse)
-                    if('QUOTED-PRINTABLE' in vcf_data_list[i]):
-                        #print(i)
-                        adresse.append(decode_adr(vcf_data_list,i))
-   #Kontakt_new.append(tempkontakt)
+                if (
+                    vcf_data_list[i][0] + (vcf_data_list[i][1]) + (vcf_data_list[i][2])
+                ) == "ADR":  # sucht Positionen die mit ADR beginnen in Liste (Zeichen für Adresse)
+                    if "QUOTED-PRINTABLE" in vcf_data_list[i]:
+                        # print(i)
+                        adresse.append(decode_adr(vcf_data_list, i))
+                    # Kontakt_new.append(tempkontakt)
                     else:
-                        adresse.append(vcf_data_list[i].split(';')[3])
-                    #print(i)
-                    #print(len(adresse))
-                    if len(adresse) ==2 :
+                        adresse.append(vcf_data_list[i].split(";")[3])
+                    # print(i)
+                    # print(len(adresse))
+                    if len(adresse) == 2:
 
                         x = i
-                        while not vcf_data_list[x][:3] in 'TEL':
-                            x = x-1
-                        tel =  vcf_data_list[x].split(':')[1]
-                        name = vcf_data_list[x-1].split(':')[1]
-                        ort = adresse[1].split(' ')
-                        straße_l = adresse[0].split(' ')
-                        street_name = ''
-                        Ort_name = ''
+                        while not vcf_data_list[x][:3] in "TEL":
+                            x = x - 1
+                        tel = vcf_data_list[x].split(":")[1]
+                        name = vcf_data_list[x - 1].split(":")[1]
+                        ort = adresse[1].split(" ")
+                        straße_l = adresse[0].split(" ")
+                        street_name = ""
+                        Ort_name = ""
                         for t in ort:
                             if t.isdigit():
                                 plz = t
                             else:
-                                Ort_name = Ort_name + ' ' + t
+                                Ort_name = Ort_name + " " + t
                         for t in straße_l:
                             if t.isdigit():
                                 hausnummer_i = t
                             else:
-                                street_name = street_name + ' ' + t
+                                street_name = street_name + " " + t
                         data = {
-                            'Name': [name],
-                            'Tel': [tel],
-                            'Street': [street_name],
-                            'Hausnummer': [hausnummer_i],
-                            'Ort': [Ort_name],
-                            'Plz': [plz]
+                            "Name": [name],
+                            "Tel": [tel],
+                            "Street": [street_name],
+                            "Hausnummer": [hausnummer_i],
+                            "Ort": [Ort_name],
+                            "Plz": [plz],
                         }
-                        #print(name)
-                        new = kontakt_new_test(name,dfKontakte)
+                        # print(name)
+                        new = kontakt_new_test(name, dfKontakte)
                         if new:
-                            print('neuer Kontakt gefunden')
+                            print("neuer Kontakt gefunden")
                             df2 = pd.DataFrame(data)
-                         #   print(df2)
+                            #   print(df2)
                             dfKontakte = dfKontakte.append(df2)
-                        #print(dfKontakte)
+                        # print(dfKontakte)
                         adresse = []
             except Exception as e:
                 print(e)
                 # print(name)
-                #print('df2')
-            i = i+1
-    dfKontakte.to_csv(csv_file,index = False)
+                # print('df2')
+            i = i + 1
+    dfKontakte.to_csv(csv_file, index=False)
     return dfKontakte
     x = 0
-def kontakt_new_test(name,df_Kontakte):
-    #kontrolliert ob der neue Kontakt bereits vorhanden ist und
+
+
+def kontakt_new_test(name, df_Kontakte):
+    # kontrolliert ob der neue Kontakt bereits vorhanden ist und
     # gibt dementsprechend True oder False zurück
-    kontakt_vorhanden = False;
+    kontakt_vorhanden = False
     for i, row in df_Kontakte.iterrows():
-        #print(row)
-        if row.get('Name') == name:
-            print('der Kontakt ist bereits vorhanden')
+        # print(row)
+        if row.get("Name") == name:
+            print("der Kontakt ist bereits vorhanden")
             kontakt_vorhanden = True
     if not kontakt_vorhanden:
-        print('neuen Kontakt speichern')
+        print("neuen Kontakt speichern")
         return True
     else:
         return False
-#_____________fals noch keine csv vorhanden ist wird diese Hier erstellt
-#prüfung ob vorhanden in in arbeit
+
+
+# _____________fals noch keine csv vorhanden ist wird diese Hier erstellt
+# prüfung ob vorhanden in in arbeit
 def create_new_csv():
-    print('create new csv')
-    with open(csv_file, 'w', encoding='utf-8') as csv_datei:
-        writer = csv.writer(csv_datei, delimiter=',')
-        writer.writerow(['Name','Nummer', 'Straße', 'Hausnummer','Ort','Plz'])
+    print("create new csv")
+    with open(csv_file, "w", encoding="utf-8") as csv_datei:
+        writer = csv.writer(csv_datei, delimiter=",")
+        writer.writerow(["Name", "Nummer", "Straße", "Hausnummer", "Ort", "Plz"])
+
+
 def loade_csv():
-    print('loade csv')
-    return pd.read_csv('Kontakte.csv')
+    print("loade csv")
+    return pd.read_csv("Kontakte.csv")
+
+
 def main():
-    #create_new_csv()
-    #dfkontakte = vcf_read()
+    # create_new_csv()
+    # dfkontakte = vcf_read()
     dfkontakte = loade_csv()
     osm_ids_kontakte = geo_coding.get_osm_id(dfkontakte)
     date_dict = osm.osm_main(osm_ids_kontakte)
-    osm.print_per_uid(date_dict)
+    osm.print_per_uid_ptty(name=True)
+
 
 main()
--- osm.py
+++ osm.py
@@ -7,22 +7,17 @@
 #   Der Methodenaufruf <print_per_uid_ptty(name=True)> ist nicht notwendig und nur zur Veranschaulichung der erhaltenen Daten.
 # ----------------------------------------------------------------------------------
 
+
 def osm_main(osm_id_list):
-    print('osm_main_start')
+    print("osm_main()")
     return search_near_by(osm_id_list)
-# TODO fixes:
-# - somethings I've missed :)
+
 
 print("\n\n" + "<" + "=" * 150 + ">")
 
 import requests
 import json
-
-overpass_url = "http://overpass-api.de/api/interpreter"
-
-search_radius = 500
-max_search_radius = 2000
-include_shops = False
+import time
 
 # user id's (or search id's, probably a better name :) TODO rename
 uids = {
@@ -35,9 +30,18 @@ uids = {
 }  # obtained from Fabi, this is just for sample testing
 
 # uids = {678537289, 3870914569}  # for request easy testing TODO remove
+
 results = {}  # dict<uid: query_result["elements"]>
+
+
 def search_near_by(osm_id_list):
-    print('start search near by')
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    search_radius = 500
+    max_search_radius = 2000
+    include_shops = False
+    global results
+
+    print(f"search_near_by(osm_id_list={osm_id_list})")
     include_searchplace = False  # should stay that way
     sp = ";nwr._->.res;out center;" if include_searchplace else "->.res;"
 
@@ -45,6 +49,7 @@ def search_near_by(osm_id_list):
     for uid in osm_id_list:
         search_radius = 500  # resetting search_radius
         while search_radius < max_search_radius:
+            print(f"For uid={uid}: sending query with radius={search_radius}")
             overpass_query = (
                 f"[out:json][timeout:500];nwr(id:{uid}){sp}(nwr[amenity=bar](around.res:{search_radius});nwr[amenity=pub](around.res:{search_radius});nwr[amenity=biergarten](around.res:{search_radius});"
                 + (
@@ -56,13 +61,7 @@ def search_near_by(osm_id_list):
             )
             try:
                 response = requests.get(overpass_url, params={"data": overpass_query})
-
-                try:
-                    result = response.json()
-                    with open("query.json", "w") as f:
-                        f.write(result)
-                except:
-                    pass
+                result = response.json()
             except:
                 print(
                     f"Exception occured during data query: {response.status_code}"
@@ -71,6 +70,8 @@ def search_near_by(osm_id_list):
                         if response.status_code == 429
                         else " (Bad request)"
                         if response.status_code == 400
+                        else " (Gateway Timeout)"
+                        if response.status_code == 504
                         else " (unknown cause :)"
                     )
                 )  # 400 is bad request, 429 is too many requests, 200 is ok
@@ -81,6 +82,7 @@ def search_near_by(osm_id_list):
             # dealing with empty result
             if result["elements"] == []:
                 search_radius += 500  # expanding search radius
+                time.sleep(1)
             else:
                 # print("debug", result)
                 break  # stops further querying
@@ -92,7 +94,9 @@ def search_near_by(osm_id_list):
         )
 
 
+# TODO remove or fix
 def print_per_uid():
+    print("print_per_uid()")
     for uid in uids:
         print(
             f"(uid={uid}):",
@@ -103,57 +107,66 @@ def print_per_uid():
 
 
 def print_per_uid_ptty(name=False):  # pretty prints the results
+    print(f"print_per_uid_ptty(name={name})")
     for uid in uids:
         print(f"(uid={uid}):")
         try:
-            for element in results[uid]:
-                if name:
-                    print(
-                        "\t{} (eid={})".format(element["tags"]["name"], element["id"]),
-                        end="",
-                    )
-                    try:
-                        found_addr = False
-                        addr_string = ', Address: "'
-                        print(addr_string, end="")
-                        addr = {  # possible address components
-                            "city": "_",
-                            "country": "_",
-                            "housenumber": "_",
-                            "postcode": "_",
-                            "street": "_",
-                        }
-                        tags = element["tags"]
-                        for tag_key, _ in tags.items():
-                            if tag_key[:4] == "addr":  # finds address components
-                                found_addr = True
-                                addr[tag_key[5:]] = tags[tag_key]
-                                # print("debug:", addr, tag_key)
-                            else:
-                                pass  # key isn't part of an address
-                        if found_addr:
+            if type(results[uid]) == str:  # ...means there weren't any results
+                print(f"\t{results[uid]}")
+            else:
+                for element in results[uid]:
+                    if name:
+                        try:
                             print(
-                                addr["street"],
-                                addr["housenumber"],
-                                addr["postcode"],
-                                addr["city"],
-                                end='"\n',
-                            )
-                        else:
-                            print('No Address given."')
-                    except:
-                        # print("Element doesn't have tags")
-                        pass
-                else:
-                    pass
-                    # print("\t(eid={}): {}".format(element.get("id"), element))
-        except:
-            print(f"\t{results[uid]}")  # when there are no results
+                                "\t{} (eid={})".format(
+                                    element["tags"]["name"], element["id"]
+                                ),
+                                end="",
+                            )  # TODO check if putting it in here was neccessary... befor it was directly after name
+                            found_addr = False
+                            addr_string = ', Address: "'
+                            print(addr_string, end="")
+                            addr = {  # possible address components
+                                "city": "_",
+                                "country": "_",
+                                "housenumber": "_",
+                                "postcode": "_",
+                                "street": "_",
+                            }
+                            tags = element["tags"]
+                            for tag_key, _ in tags.items():
+                                if tag_key[:4] == "addr":  # finds address components
+                                    found_addr = True
+                                    addr[tag_key[5:]] = tags[tag_key]
+                                    # print("debug:", addr, tag_key)
+                                else:
+                                    pass  # key isn't part of an address
+                            if found_addr:
+                                print(
+                                    addr["street"],
+                                    addr["housenumber"],
+                                    addr["postcode"],
+                                    addr["city"],
+                                    end='"\n',
+                                )
+                            else:
+                                print('No Address given."')
+                        except:
+                            # print("Element doesn't have tags")
+                            print(f"\tError occured with element:{element}")
+                            pass
+                    else:
+                        print("\t(eid={}): {}".format(element.get("id"), element))
+        except Exception as e:
+            print(f"Exception occured while printing results: {e}")
+            # print(f"\t{results[uid]}")  # when there are no results
 
         print("-" * 50)
 
 
-
+if __name__ == "__main__":
+    osm_main(uids)
+    print_per_uid_ptty(name=True)
 
 # debugging
 # print(results)
